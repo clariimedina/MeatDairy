272f8275cfad67bd933b36e0deead54a3eb8867e
biancalopez <bisela92@gmail.com>
2014-09-16 14:04:06 -0600

    Contenedores en productos.php

--- templates/carrusel.php
+++ templates/carrusel.php
@@ -1,44 +1,44 @@
 <div id="body-container">
-<section id="body-carousel-container">
-    <div id="myCarousel" class="carousel slide carousel1">
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-            <li data-target="#myCarousel" data-slide-to="3"></li>
-            <li data-target="#myCarousel" data-slide-to="4"></li>
-        </ol>
-        <!-- Carousel items-->
-        <div class="carousel-inner">
-            <div class="active item"><img src="images/imagenesEntrada/vaca1.jpg" alt="banner1" /></div>
-            <div class="item"><img src="images/imagenesEntrada/queso1.jpg" alt="banner2"/></div>
-            <div class="item"><img src="images/imagenesEntrada/vaca5.png" alt="banner3"/></div>
-            <div class="item"><img src="images/imagenesEntrada/queso2.jpg" alt="banner4"/></div>
-            <div class="item"><img src="images/imagenesEntrada/vaca4.jpg" alt="banner5"/></div>
-            <div class="item"><img src="images/imagenesEntrada/queso3.jpg" alt="banner5"/></div>
-        </div>
-        <!--<div class="carousel-caption">
-        <!-- Carousel nav 
-            <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-            <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-        </div>-->
-    </div><!--End div myCarousel-->	
-</section>
+    <section id="body-carousel-container">
+        <div id="myCarousel" class="carousel slide carousel1">
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+                <li data-target="#myCarousel" data-slide-to="4"></li>
+            </ol>
+            <!-- Carousel items-->
+            <div class="carousel-inner">
+                <div class="active item"><img src="images/imagenesEntrada/vaca1.jpg" alt="banner1" /></div>
+                <div class="item"><img src="images/imagenesEntrada/queso1.jpg" alt="banner2"/></div>
+                <div class="item"><img src="images/imagenesEntrada/vaca5.png" alt="banner3"/></div>
+                <div class="item"><img src="images/imagenesEntrada/queso2.jpg" alt="banner4"/></div>
+                <div class="item"><img src="images/imagenesEntrada/vaca4.jpg" alt="banner5"/></div>
+                <div class="item"><img src="images/imagenesEntrada/queso3.jpg" alt="banner5"/></div>
+            </div>
+            <!--<div class="carousel-caption">
+            <!-- Carousel nav 
+                <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+            </div>-->
+        </div><!--End div myCarousel-->	
+    </section>
 
-<section id="body-images-container">
-   <ul class="body-images-list">
-        <li class="body-images-col left-space">
-            <img class="body-images" src="images/ImagenesInicio/lacteos1.jpg">
-            <p>Contamos con productos <br> de la mas alta calidad</p>
-        </li>
-        <li class="body-images-col left-space">
-            <img class="body-images" src="images/ImagenesInicio/desayunando.jpg">
-            <p>Siempre pensando en lo <br> mejor para tu familia</p>
-        </li>
-        <li class="body-images-col left-space">
-            <img class="body-images" src="images/ImagenesInicio/vacas.jpg">
-            <p>Empresa socialmente <br> responsable</p>
-        </li>
-    </ul>
-</section>
+    <section id="body-images-container">
+       <ul class="body-images-list">
+            <li class="body-images-col left-space">
+                <img class="body-images" src="images/ImagenesInicio/lacteos1.jpg">
+                <p>Contamos con productos <br> de la mas alta calidad</p>
+            </li>
+            <li class="body-images-col left-space">
+                <img class="body-images" src="images/ImagenesInicio/desayunando.jpg">
+                <p>Siempre pensando en lo <br> mejor para tu familia</p>
+            </li>
+            <li class="body-images-col left-space">
+                <img class="body-images" src="images/ImagenesInicio/vacas.jpg">
+                <p>Empresa socialmente <br> responsable</p>
+            </li>
+        </ul>
+    </section>
 </div>
