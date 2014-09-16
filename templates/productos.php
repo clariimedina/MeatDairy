fd4cfcd40a00889d92bfc612067f5cddfe04a1b4
ArianWolf <pepezhe2@hotmail.com>
2014-09-16 14:48:29 -0600

    hola

--- templates/productos.php
+++ templates/productos.php
@@ -1,6 +1,17 @@
 <section id="body-product-container">
     <div id="product-left-container"></div>
-    <div id="product-right-container"></div>
+    <div id="product-right-container">
+
+
+
+    </div>
+    <button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="left" title="Tooltip on left">Tooltip on left</button>
+
+<button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="top" title="Tooltip on top">Tooltip on top</button>
+
+<button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" title="Tooltip on bottom">Tooltip on bottom</button>
+
+<button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="right" title="Tooltip on right">Tooltip on right</button>
 </section>
 <script type="text/javascript" src="js/acordeonProductos.js"></script>
 <script type="text/javascript" src="js/seleccionadoProductos.js"></script>
