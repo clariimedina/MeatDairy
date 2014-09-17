77f5043bec04d28fc6693cb5685717ee04f59df7
biancalopez <bisela92@gmail.com>
2014-09-17 01:34:31 -0600

    productos.php terminado

--- templates/productos.php
+++ templates/productos.php
@@ -1,10 +1,30 @@
 <section id="body-product-container">
     <div id="product-left-container">
-        <ul>
-            <li>Ganado</li>
-            <li>Cortes</li>
-            <li>Lácteos</li>
-        </ul>
+        <dl class="shadow">
+            <dt class="product-menu-font1 product-title-menu-background">Menu</dt>
+            
+            <dt><a class="product-menu-font2" href="#">Ganado</a></dt>
+            <dd>
+                <ul>
+                    <li class="shadow"><a id="angus" class="list-element product-menu-font3" href="#">Angus</a></li>
+                    <li class="shadow"><a id="beefmaster" class="list-element product-menu-font3" href="#">Beefmaster</a></li>
+                    <li class="shadow"><a id="belmontRed" class="list-element product-menu-font3" href="#">BelmontRed</a></li>
+                    <li class="shadow"><a id="tudanca" class="list-element product-menu-font3" href="#">Tudanca</a></li>
+                </ul>
+            </dd>
+
+            <dt><a class="product-menu-font2" href="#">Cortes</a></dt>
+            
+            <dt><a class="product-menu-font2" href="#">Lácteos</a></dt>
+            <dd>
+                <ul>
+                    <li class="shadow"><a id="crema" class="list-element product-menu-font3" href="#">Crema</a></li>
+                    <li class="shadow"><a id="leche" class="list-element product-menu-font3" href="#">Leche</a></li>
+                    <li class="shadow"><a id="mantequilla" class="list-element product-menu-font3" href="#">Mantequilla</a></li>
+                    <li class="shadow"><a id="queso" class="list-element product-menu-font3" href="#">Queso</a></li>
+                </ul>
+            </dd>
+        </dl>
     </div> <!--End product-left-container-->
 
 
