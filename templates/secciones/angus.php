0752bb0a1cda5ba09c3e7e6d4f3c3c672eac4535
biancalopez <bisela92@gmail.com>
2014-09-17 13:51:19 -0600

    descargar vaquitas

--- templates/secciones/angus.php
+++ templates/secciones/angus.php
@@ -1,5 +1,3 @@
-<!-- seccion de para mostrar el ganado de raza angus -->
-
 <section>
     <ul id="lista-prod" >
 
@@ -24,50 +22,22 @@ $(document).ready(function(response) {
                         codigo += '<a class="fancybox" rel="gallery1" href="images/imagenesProductos/'+ganado.foto+'" title="">';
                         codigo += '<img width=200 src="images/imagenesProductos/'+ganado.foto+'">';
                         codigo += '</a>';
-                        codigo += '<button type="button" class="btn btn-lg btn-info" data-toggle="popover"';
-                        codigo += 'data-placement="bottom" title="Vaquita" data-content="';
-                        codigo += '<h4>'+ganado.nombre+'</h4>';
-                        codigo += '<h6>'+ganado.peso+'</h6>';
-                        codigo += '<h6>'+ganado.precio+'</h6>';
-                        codigo += '<h6>'+ganado.raza+'</h6>">Info';
-                        codigo += '</button>';
-                        codigo += '<button id="btn-pdf"onclick="generarPDF('+ganado.id+')">Generar PDF</button>';
+                        codigo += '<button type="button" class="btn btn-lg btn-info" data-toggle="popover" data-placement="bottom" title="Vaquita" data-content="<h4>'+ganado.nombre+'</h4> <h6>'+ganado.peso+'</h6> <h6>'+ganado.precio+'</h6> <h6>'+ganado.raza+'</h6>">Info </button>';
+                        codigo += '<button id="btn-pdf"onclick="generarPDF('+ganado.id+')">PDF</button>';
                         codigo += '</div>';
                         codigo += '</li>';
-/*
-                        codigo += '<li style="list-style:none; display: inline-block;">';
-                        codigo += '<div style="width:60%;">';
-                        codigo += '<a class="fancybox" rel="gallery1" href="images/imagenesProductos/'+ganado.foto+'" title="">';
-                        codigo += '<img width=200 src="images/imagenesProductos/'+ganado.foto+'">';
-                        codigo += '</a>';
-                        codigo += '<button  type="button"  class="btn btn-lg btn-info" data-toggle="popover" 
-                                         data-placement="bottom" title="Vaquita" data-content="        
-                                        <h4> Nombre: '+ganado.nombre+'</h4>
-                                        <h6> Peso:'+ganado.peso+'</h6>
-                                        <h6> Precio:'+ganado.precio+'</h6>
-                                        <h6> Raza:'+ganado.raza+'</h6>">Info';
-                        codigo += '</button>';
-                        codigo += '<button onclick="generarPDF('+ganado.id+')">Generar PDF</button>';
-                        codigo += '</div>';
-                        codigo += '</li>';
-*/
 
                     });
                     $('#lista-prod').html(codigo);
                 }
                 else {
+                    }
 
-                }
-            }
-        });
-});
-</script>
+                 }   
 
-<script type="text/javascript" src="js/pruebapdf.js"></script>
-<script type="text/javascript">$(function (){ 
-    alert("djkallkjsada");
-    $('[data-toggle="popover"]').popover('show');
     });
-</script>
 
-                  
+});
+
+</script>
+<script>$(function () { $("[data-toggle='popover']").popover();  });</script>
